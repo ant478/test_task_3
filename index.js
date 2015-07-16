35af51f0e629db6dd1c037d194352d219dc287f8
gena <ant478@gmail.com>
2015-07-16 21:22:12 +0300

    did some shit

---
+++ index.js
@@ -0,0 +1,12 @@
+  $(document).ready(function () {
+
+    var swiper1 = new Swiper('.swiper1 .swiper-container', {
+      nextButton: '.swiper1 .swiper-button-next',
+      prevButton: '.swiper1 .swiper-button-prev',
+    });
+
+    var swiper2 = new Swiper('.swiper2 .swiper-container', {
+      nextButton: '.swiper2 .swiper-button-next',
+      prevButton: '.swiper2 .swiper-button-prev',
+    });  
+  });
